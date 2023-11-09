2198e95ca20222822f9f10f95074c7e5ab8b418d
2020147542 <tmfal5023@yonsei.ac.kr>
2023-11-10 02:38:32 +0900

    build 성공위함

--- src/main/java/com/YCtechAcademy/bogosaja/config/CacheConfig.java
+++ src/main/java/com/YCtechAcademy/bogosaja/config/CacheConfig.java
@@ -1,37 +1,37 @@
-package com.YCtechAcademy.bogosaja.config;
-
-
-import com.YCtechAcademy.bogosaja.item.domain.Item;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-@EnableCaching
-@RequiredArgsConstructor
-public class CacheConfig {
-
-    private final RedisProperties redisProperties;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
-    }
-
-    @Bean
-    public RedisTemplate<String, Item> postRedisTemplate() {
-        RedisTemplate<String, Item> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Item.class));
-        return redisTemplate;
-    }
-
-}
+//package com.YCtechAcademy.bogosaja.config;
+//
+//
+//import com.YCtechAcademy.bogosaja.item.domain.Item;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//@Configuration
+//@EnableCaching
+//@RequiredArgsConstructor
+//public class CacheConfig {
+//
+//    private final RedisProperties redisProperties;
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory(){
+//        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Item> postRedisTemplate() {
+//        RedisTemplate<String, Item> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Item.class));
+//        return redisTemplate;
+//    }
+//
+//}
